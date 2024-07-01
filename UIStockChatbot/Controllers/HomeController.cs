029a1691a3f4efe7d1df0b2367be78b2ced218a0
NovaTechset <bikumalla.sampath@gmail,com>
2024-07-02 00:07:30 +0530

    LSEG chatbot

---
+++ UIStockChatbot/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using UIStockChatbot.Services;
+
+namespace UIStockChatbot.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StockService _stockService;
+
+        public HomeController()
+        {
+            _stockService = new StockService();
+        }
+
+        public ActionResult Index()
+        {
+            var stockExchanges = _stockService.GetStockExchanges();
+            if (stockExchanges == null || !stockExchanges.Any())
+            {
+                ViewBag.ErrorMessage = "No stock exchanges found.";
+            }
+            return View(stockExchanges);
+        }
+
+        public ActionResult StockMenu(string code)
+        {
+            var stockExchange = _stockService.GetStockExchangeByCode(code);
+            if (stockExchange == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockExchange);
+        }
+
+        public ActionResult StockDetails(string exchangeCode, string stockCode)
+        {
+            var stockExchange = _stockService.GetStockExchangeByCode(exchangeCode);
+            if (stockExchange == null)
+            {
+                return HttpNotFound();
+            }
+            var stock = stockExchange.TopStocks.FirstOrDefault(s => s.Code.Equals(stockCode, StringComparison.OrdinalIgnoreCase));
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+    }
+}
---
+++ UIStockChatbot/Services/StockService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using UIStockChatbot.Models;
+
+namespace UIStockChatbot.Services
+{
+    public class StockService
+    {
+        private readonly List<StockExchange> _stockExchanges;
+
+        public StockService()
+        {
+            var jsonFile = Path.Combine(HttpRuntime.AppDomainAppPath, "App_Data/Chatbot-stockdata.json");
+            var json = File.ReadAllText(jsonFile);
+            _stockExchanges = JsonConvert.DeserializeObject<List<StockExchange>>(json);
+        }
+
+        public List<StockExchange> GetStockExchanges() => _stockExchanges;
+
+        public StockExchange GetStockExchangeByCode(string code) =>
+            _stockExchanges.FirstOrDefault(se => se.Code.Equals(code, StringComparison.OrdinalIgnoreCase));
+    }
+}
